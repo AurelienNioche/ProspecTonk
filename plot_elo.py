8b2f7bce349dce4ff9fbe33ec41378c2a0de9d4c
AurelienNioche <nioche.aurelien@gmail.com>
2020-11-30 19:20:07 +0200

    update

--- plot_elo.py
+++ plot_elo.py
@@ -7,15 +7,15 @@ data_path = os.path.join(SOURCE_FOLDER, "elo_matrix_ProspecTonk.xlsx")
 
 print(f"Reading from '{data_path}'...", end=" ", flush=True)
 df = pd.read_excel(data_path)
-
-monkeys = 'Abr', 'Ala', 'Bar', 'Ces', 'Dor', 'Nem', 'Pac', 'Yoh'
-
-fig, ax = plt.subplots()
-for m in monkeys:
-    m = m.lower()
-    ax.plot(df[m], label=m)
-
-ax.set_xlabel('time')
-ax.set_ylabel('elo rating')
-plt.legend()
-plt.savefig(os.path.join(FIG_FOLDER, "elo.pdf"))
+df.date = pd.to_datetime(df.date)
+# monkeys = 'Abr', 'Ala', 'Bar', 'Ces', 'Dor', 'Nem', 'Pac', 'Yoh'
+#
+# fig, ax = plt.subplots()
+# for m in monkeys:
+#     m = m.lower()
+#     ax.plot(df[m], label=m)
+#
+# ax.set_xlabel('time')
+# ax.set_ylabel('elo rating')
+# plt.legend()
+# plt.savefig(os.path.join(FIG_FOLDER, "elo.pdf"))
